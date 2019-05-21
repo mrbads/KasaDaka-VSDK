51664a627fdc688fd46671b193075c39ca42908a
Bas van der Borden <bascasper@gmail.com>
2019-05-21 15:48:50 +0200

    generic views update

--- vsdk/service_development/urls.py
+++ vsdk/service_development/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 app_name= 'service-development'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    path('<str:category_name>/', views.detail, name='detail'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     url(r'^choice/(?P<element_id>[0-9]+)/(?P<session_id>[0-9]+)$', views.choice, name='choice'),
     url(r'^message/(?P<element_id>[0-9]+)/(?P<session_id>[0-9]+)$', views.message_presentation, name='message-presentation'),
     url(r'^start/(?P<voice_service_id>[0-9]+)$', views.voice_service_start, name='voice-service'),
--- vsdk/service_development/views/webint.py
+++ vsdk/service_development/views/webint.py
@@ -1,24 +1,48 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
 
 from ..models import UserInputCategory, SpokenUserInput
 
-def index(request):
-    category_list = UserInputCategory.objects.all()
-    context = { 'category_list': category_list, }
-    return render(request, 'webint.html', context)
+class IndexView(generic.ListView):
+    """docstring for IndexView."""
+    template_name = 'webint.html'
+    context_object_name = 'category_list'
 
-def detail(request, category_name):
-    name = category_name
-    category = UserInputCategory.objects.get(name=category_name)
-    return render(request, 'detail.html', {'category': category, 'name': name})
+    def get_queryset(self):
+        return UserInputCategory.objects.all()
+
+# def index(request):
+#     category_list = UserInputCategory.objects.all()
+#     context = { 'category_list': category_list, }
+#     return render(request, 'webint.html', context)
+
+class DetailView(generic.DetailView):
+    """docstring for DetailView."""
+    model = UserInputCategory
+    template_name = 'detail.html'
+
+    def get_queryset(self):
+        return UserInputCategory.objects.all()
+
+# def detail(request, category_name):
+#     category = get_object_or_404(UserInputCategory, name=category_name)
+#     return render(request, 'detail.html', {
+#         'category': category,
+#         'name': category_name
+#         })
 
 def delete(request, category_name):
-    category = UserInputCategory.objects.get(name=category_name)
+    category = get_object_or_404(UserInputCategory, name=category_name)
     try:
-        selected_order = UserInputCategory.spokenuserinput_set.get(pk=request.POST['order'])
+        selected_order = category.category.get(pk=request.POST['order'])
     except (KeyError, SpokenUserInput.DoesNotExist):
-        return render(request, 'detail.html', {'category': category, 'name': category_name, 'error_message': "You didn't select a order"})
+        return render(request, 'detail.html', {
+            'category': category,
+            'name': category_name,
+            'error_message': "You didn't select a order"
+        })
     else:
         selected_order.delete()
         return HttpResponseRedirect(reverse('detail.html', args=(category_name)))
