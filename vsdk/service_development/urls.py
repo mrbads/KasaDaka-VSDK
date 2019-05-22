b9d4cf7df4b10cbf4ec2dd21cf9bc1859244031d
Bas van der Borden <bascasper@gmail.com>
2019-05-22 21:55:58 +0200

    delete update #2

--- vsdk/service_development/urls.py
+++ vsdk/service_development/urls.py
@@ -7,6 +7,7 @@ app_name= 'service-development'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/del/', views.delete, name='delete'),
     url(r'^choice/(?P<element_id>[0-9]+)/(?P<session_id>[0-9]+)$', views.choice, name='choice'),
     url(r'^message/(?P<element_id>[0-9]+)/(?P<session_id>[0-9]+)$', views.message_presentation, name='message-presentation'),
     url(r'^start/(?P<voice_service_id>[0-9]+)$', views.voice_service_start, name='voice-service'),
